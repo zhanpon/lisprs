6364989ca1b7f685e6f9f978b053cbec64aae90e
Yiyang Zhan <pon.zhan@gmail.com>
2023-03-04 00:24:32 +0900

    refactor: tokenizer

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -14,16 +14,17 @@ impl<'a> Iterator for Tokenizer<'a> {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.remaining.is_empty() {
+        let s = self.remaining;
+        if s.is_empty() {
             return None;
         }
 
-        let (token, remaining) = if self.remaining.starts_with(['(', ')']) {
-            self.remaining.split_at(1)
-        } else if let Some(i) = self.remaining.find([' ', '(', ')']) {
-            self.remaining.split_at(i)
+        let (token, remaining) = if s.starts_with(['(', ')']) {
+            s.split_at(1)
+        } else if let Some(i) = s.find([' ', '(', ')']) {
+            s.split_at(i)
         } else {
-            (self.remaining, "")
+            (s, "")
         };
 
         self.remaining = remaining.trim_start();
